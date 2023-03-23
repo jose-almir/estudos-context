f1712be28167c453de38183a48a10a8e850463ff
jose-almir <jr.cod.dev@gmail.com>
2023-03-23 12:03:38 -0300

    Props Drilling

--- src/components/CompFilhoE/CompFilhoE.jsx
+++ src/components/CompFilhoE/CompFilhoE.jsx
@@ -1,5 +1,13 @@
 import "./CompFilhoE.css";
 
-export function CompFilhoE() {
-  return <div className="filho-e"></div>;
+export function CompFilhoE(props) {
+  return (
+    <div className="filho-e">
+      <h3>FILHO E</h3>
+      <p>
+        <b>Valor: </b>
+        {props.valor}
+      </p>
+    </div>
+  );
 }
--- src/components/CompPai/CompPai.jsx
+++ src/components/CompPai/CompPai.jsx
@@ -1,5 +1,31 @@
+import { useState } from "react";
+import { CompFilhoA } from "../CompFilhoA/CompFilhoA";
+import { CompFilhoB } from "../CompFilhoB/CompFilhoB";
 import "./CompPai.css";
 
 export function CompPai() {
-  return <div className="pai"></div>;
+  // Primeiramente definimos uma informação
+  // que os componentes da hierarquia irão precisar
+  const [batata, setBatata] = useState("batata");
+
+  function repetir() {
+    setBatata(batata + "a");
+  }
+  // Props drilling = quando temos uma hierarquia
+  // de componentes muito extensa para passar props
+  // em cada componente
+  return (
+    <div className="pai">
+      <h3>PAI</h3>
+      <button className="btn btn-dark" onClick={repetir}>
+        Repetir
+      </button>
+      <p>
+        <b>Valor: </b>
+        {batata}
+      </p>
+      <CompFilhoA valor={batata} />
+      <CompFilhoB />
+    </div>
+  );
 }
