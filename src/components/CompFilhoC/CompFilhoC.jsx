a1facb8124b06ff2ebf972eea4e54adb6d3b4f6e
jose-almir <jr.cod.dev@gmail.com>
2023-03-23 15:03:55 -0300

    Atividade I

--- src/components/CompFilhoC/CompFilhoC.jsx
+++ src/components/CompFilhoC/CompFilhoC.jsx
@@ -2,12 +2,12 @@ import { CompFilhoD } from "../CompFilhoD/CompFilhoD";
 import { CompFilhoE } from "../CompFilhoE/CompFilhoE";
 import "./CompFilhoC.css";
 
-export function CompFilhoC(props) {
+export function CompFilhoC() {
   return (
     <div className="filho-c">
       <h3>FILHO C</h3>
       <CompFilhoD />
-      <CompFilhoE valor={props.valor} />
+      <CompFilhoE />
     </div>
   );
 }
