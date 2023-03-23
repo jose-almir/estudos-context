9aaa2cba3354dccaf686d61c37fa5d832ac28d99
jose-almir <jr.cod.dev@gmail.com>
2023-03-23 15:56:46 -0300

    Inicio Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import { CompPai } from "./components/CompPai/CompPai";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { NavBar } from "./components/NavBar/NavBar";
+import { Home } from "./pages/Home/Home";
+import { Login } from "./pages/Login/Login";
+import { Perfil } from "./pages/Perfil/Perfil";
 
 export function App() {
   return (
-    <div>
-      <CompPai />
-    </div>
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/perfil" element={<Perfil />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,3 +1,27 @@
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
 export function NavBar() {
-  return <></>;
+  return (
+    <Navbar bg="success" expand="sm">
+      <Container fluid>
+        <Navbar.Brand>App</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse>
+          <Nav>
+            <Nav.Link as={Link} to="/">
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} to="/perfil">
+              Perfil
+            </Nav.Link>
+            <Nav.Link as={Link} to="/login">
+              Login
+            </Nav.Link>
+            <Button variant="outline-light">Alternar</Button>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
