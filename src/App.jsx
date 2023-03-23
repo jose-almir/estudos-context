50c1ae723c7557898e401bba9a151ed341cb5978
jose-almir <jr.cod.dev@gmail.com>
2023-03-23 17:20:53 -0300

    Finalização Tema Escuro

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,37 @@ import { NavBar } from "./components/NavBar/NavBar";
 import { Home } from "./pages/Home/Home";
 import { Login } from "./pages/Login/Login";
 import { Perfil } from "./pages/Perfil/Perfil";
+import { ThemeContext } from "./contexts/ThemeContext";
+import { useState } from "react";
 
 export function App() {
+  // true = tema escuro ativo
+  // false = tema claro ativo
+  const [temaEscuro, setTemaEscuro] = useState(false);
+
+  function alternar() {
+    // Alternar entre true e false toda vez
+    // que for chamada
+    if (temaEscuro === true) {
+      setTemaEscuro(false);
+    } else {
+      setTemaEscuro(true);
+    }
+  }
+
   return (
-    <BrowserRouter>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/perfil" element={<Perfil />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeContext.Provider
+      value={{ temaEscuro: temaEscuro, alternar: alternar }}
+    >
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/perfil" element={<Perfil />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeContext.Provider>
   );
 }
 
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,24 @@
+import { useContext } from "react";
 import { Button, Container, Nav, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { ThemeContext } from "../../contexts/ThemeContext";
 
 export function NavBar() {
+  const resultado = useContext(ThemeContext);
+  const temaEscuro = resultado.temaEscuro;
+  const alternar = resultado.alternar;
+
+  let iconeBtn = "https://cdn-icons-png.flaticon.com/512/3073/3073665.png";
+  if (temaEscuro) {
+    iconeBtn = "https://cdn-icons-png.flaticon.com/512/581/581601.png";
+  }
+
   return (
-    <Navbar bg="success" expand="sm">
+    <Navbar
+      bg={temaEscuro ? "dark" : "success"}
+      variant={temaEscuro ? "dark" : "light"}
+      expand="sm"
+    >
       <Container fluid>
         <Navbar.Brand>App</Navbar.Brand>
         <Navbar.Toggle />
@@ -18,7 +33,10 @@ export function NavBar() {
             <Nav.Link as={Link} to="/login">
               Login
             </Nav.Link>
-            <Button variant="outline-light">Alternar</Button>
+            <Button variant="outline-light" onClick={alternar}>
+              <img src={iconeBtn} width="16" />
+              Alternar
+            </Button>
           </Nav>
         </Navbar.Collapse>
       </Container>
